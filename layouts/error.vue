6a68900d5387124ae8dd49ed6811cbc1dde130c9
J3m1th <denys.petryniak@gmail.com>
2021-12-13 21:36:25 +0200

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="text-align: center;">
+    <img
+      src="https://image.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg"
+      alt=""
+    />
+    <h1>Error!</h1>
+    {{ error.message }} {{ error.statusCode }}
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "BIG PROBLEMS",
+    };
+  },
+
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
