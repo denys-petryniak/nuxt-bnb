1166288c47b544eed527a7adca192af8a395433d
J3m1th <denys.petryniak@gmail.com>
2021-12-17 00:30:48 +0200

    Add additional property details in the search results

--- pages/search.vue
+++ pages/search.vue
@@ -1,21 +1,28 @@
 <template>
   <div>
     {{ lat }} / {{ lng }} / {{ label }} <br />
-    <div v-for="home in homes" :key="home.objectID">{{ home.title }} <br /></div>
+    <div v-if="homes.length > 0">
+      <HomeRow v-for="home in homes" :key="home.objectID" :home="home" />
+    </div>
+    <div v-else>
+      No results found
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  head() {
+    return {
+      title: `Homes around ${this.label}`,
+    };
+  },
+
   async beforeRouteUpdate(to, from, next) {
     const data = await this.$dataApi.getHomesByLocation(to.query.lat, to.query.lng);
-    
+
     this.homes = data.json.hits;
-    this.label = to.query.label,
-    this.lat = to.query.lat,
-    this.lng = to.query.lng,
-    
-    next();
+    (this.label = to.query.label), (this.lat = to.query.lat), (this.lng = to.query.lng), next();
   },
 
   async asyncData({ query, $dataApi }) {
