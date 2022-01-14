0e3cffea5b5ecc86fb160c36e7c7f55bd61123a9
J3m1th <denys.petryniak@gmail.com>
2022-01-14 21:54:38 +0200

    Refactor proxy APIs

---
+++ modules/algolia/apis/user.js
@@ -0,0 +1,35 @@
+import fetch from "node-fetch";
+import { unWrap, getErrorResponse } from "../../../utils/fetchUtils";
+import { getHeaders } from "../helpers";
+
+export default algoliaConfig => {
+  const headers = getHeaders(algoliaConfig);
+
+  return {
+    create: async (identity, payload) => {
+      try {
+        return unWrap(
+          await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
+            headers,
+            method: "PUT",
+            body: JSON.stringify(payload),
+          })
+        );
+      } catch (error) {
+        return getErrorResponse(error);
+      }
+    },
+
+    getById: async identity => {
+      try {
+        return unWrap(
+          await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
+            headers,
+          })
+        );
+      } catch (error) {
+        return getErrorResponse(error);
+      }
+    },
+  };
+};
--- modules/algolia/index.js
+++ modules/algolia/index.js
@@ -1,11 +1,11 @@
-import { getHeaders } from "./helpers";
+import getApis from "./apis";
 import userRouter from "./routers/user";
 
 export default function() {
   const algoliaConfig = this.options.privateRuntimeConfig.algolia;
-  const headers = getHeaders(algoliaConfig);
+  const apis = getApis(algoliaConfig);
 
   this.nuxt.hook("render:setupMiddleware", app => {
-    app.use("/api/user", userRouter(headers));
+    app.use("/api/user", userRouter(apis));
   });
 }
