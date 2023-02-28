128b19ee2b71b119e46be7419ce8d6dcd553e994
Denys Petryniak <denys.petryniak@gmail.com>
2023-02-28 18:23:08 +0200

    Add some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,9 @@ module.exports = {
   extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
 }
