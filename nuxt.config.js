958e47b9dee4d08d9e242ef73363c916280229fd
Denys Petryniak <denys.petryniak@gmail.com>
2023-02-28 18:47:50 +0200

    Add viewport meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,9 +13,8 @@ export default {
     },
 
     meta: [
-      {
-        charset: 'utf-8',
-      },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
 
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
